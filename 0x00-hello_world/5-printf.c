dccc97a9f7637e1f5c849e0ba51fbcc8c50b0108
sinafantu <sinafantu10@gmail.com>
2023-02-17 11:49:56 -0800

    task5 fix1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include  <stdio.h>
 
 /**
- *  main - with proper grammar, but the outcome is
- *  a piece of art, followed newline
- *  Return: 0 (Success)
+ *  main - prints " with proper grammar, but the outcome is
+ *  a piece of art," followed newline
+ *  Return: Always 0.
  *
  */
 
