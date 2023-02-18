10021f0329eaacd38a4720c02cef8b3b49f8901e
sinafantu <sinafantu10@gmail.com>
2023-02-18 07:38:27 -0800

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -33,7 +33,7 @@ lastd);
 	}
 	else if (lastd < 6 && lastd != 0)
 	{
-	printf("Last digit of %d is %d amd is less than 6 and not 0\n",
+	printf("Last digit of %d is %d and is less than 6 and not 0\n",
 n, lastd);
 	}
 	return (0);
