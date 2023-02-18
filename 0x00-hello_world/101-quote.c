466e12c38dcac4a0898db770673d2ea08ed10797
sinafantu <sinafantu10@gmail.com>
2023-02-18 13:12:31 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
 /**
- * main - prints "and that piee of art is useful" - Dora Korpar, 2015-1019",
+ * main - prints "and that piee of art is useful" - Dora Korpar, 2015-10-19",
  * followed bt a newline, to standard error
  * Return: Alwas 1.
  */
@@ -10,6 +10,6 @@ int main(void)
 
 {
 
-	write(2, "and that piece of art useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art useful\" -Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
