bfd5eaaa4175e4760ec5437370bb82c9e19bc8e0
sinafantu <sinafantu10@gmail.com>
2023-02-27 23:09:15 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-/**
- * main - Generates random valid passwords for the
- *  program 101-crackme.
- *  Return: Always 0.
+/**  
+ *    main - Generates random valid passwords for the
+ *    program 101-crackme.
+ *    Return: Always 0.
  */
 
 int main(void)
