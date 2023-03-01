aa7f8db7508bc726329373c71ee213b0a66c9581
sinafantu <sinafantu10@gmail.com>
2023-03-01 12:06:45 -0800

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *  _strncat - Copies at most an inputted number
+ *             of bytes from string src into dest.
+ *  @dest: The buffer stroing the string copy.
+ *  @src: The source string.
+ *  @n: The maximum number of bytes to copied form src.
+ *
+ *  Return: A pointer to the resulting string dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = 0; src_len; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
+}
