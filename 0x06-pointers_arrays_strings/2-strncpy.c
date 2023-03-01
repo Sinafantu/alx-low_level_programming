ba36a5fcbb76cb1bd936774d7e867ca5d7cae545
sinafantu <sinafantu10@gmail.com>
2023-03-01 12:09:14 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,13 +13,13 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int index = 0, dest_len = 0;
 
-	while (dest[index++])
+	while (src[index++])
 		src_len++;
 
 	for (index = 0; src[index] && index < n; index++)
 		dest[index] = src[index];
 
-	for (index = 0; src_len; index < n; index++)
+	for (index =  src_len; index < n; index++)
 		dest[index] = '\0';
 
 	return (dest);
