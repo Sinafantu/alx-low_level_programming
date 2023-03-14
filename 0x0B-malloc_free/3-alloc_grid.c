890f40964be7a95e6990147adc37716ecba37150
sinafantu <sinafantu10@gmail.com>
2023-03-14 08:45:05 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdlib.h>
 
 /**
@@ -7,6 +8,7 @@
  *
  * Return: ...
  */
+
 int **alloc_grid(int width, int height)
 {
 	int i, j, k, l;
@@ -46,6 +48,6 @@ int **alloc_grid(int width, int height)
 			a[k][l] = 0;
 		}
 	}
-	
+
 	return (a);
 }
