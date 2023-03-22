f8a4b3087b4be23037a6a985d686901dd31a8d06
sinafantu <sinafantu10@gmail.com>
2023-03-22 04:24:35 -0700

    3-main.c

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,9 +1,10 @@
-#ifndef CALC_H
-#define CALC_H
+#ifndef _HEADER_
+#define _HEADER_
 /**
- * struct op - A struct op.
- * @op: The operator.
- * @f: The associated function.
+ * struct op - struct op
+ *
+ * @op: The operator
+ * @f: The function associated
  */
 typedef struct op
 {
