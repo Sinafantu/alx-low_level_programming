c8b38400da9541c773118e692fb3e2214ef34b4b
sinafantu <sinafantu10@gmail.com>
2023-03-23 11:28:04 -0700

    2-print_strings.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,12 +14,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int index;
 
 	va_start(nums, n);
+
 	for (index = 0; index < n; index++)
 	{
 		printf("%d", va_arg(nums, int));
+
 		if (index != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
+
 	printf("\n");
+
 	va_end(nums);
 }
