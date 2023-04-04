6897f08b15a258efc5c0d3f0565f701a7924ce03
sinafantu <sinafantu10@gmail.com>
2023-04-03 23:51:29 -0700

    int

--- 0x13-more_singly_linked_lists/list.h
+++ 0x13-more_singly_linked_lists/list.h
@@ -8,6 +8,7 @@
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
  * for alx project
  */
@@ -34,4 +35,3 @@ size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
 #endif
-
