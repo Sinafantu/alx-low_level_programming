3d5bf230aa97acd158401cc25c7590c6fe60387c
sinafantu <sinafantu10@gmail.com>
2023-04-10 10:46:23 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,6 @@
  * @text_content: A pointer to a string to write to the file.
  *
  * Return: If the function fails - -1.
- *         Otherwise - 1.
  */
 
 int create_file(const char *filename, char *text_content);
@@ -32,4 +31,3 @@ int create_file(const char *filename, char *text_content);
 
 	return (1);
 }
-
