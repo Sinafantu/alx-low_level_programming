82e73e95c524a499f30b8a52f77f6de0b5701cb4
sinafantu <sinafantu10@gmail.com>
2023-04-10 11:06:37 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,7 +10,7 @@
  *         Otherwise - 1.
  */
 
-int append_text_to_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int o, w, len = 0;
 
