b5cee243d513cd39d6c4fbb7b1b1b10fb6e95f39
sinafantu <sinafantu10@gmail.com>
2023-04-10 10:48:05 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,9 +6,10 @@
  * @text_content: A pointer to a string to write to the file.
  *
  * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
 
-int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content)
 {
 	int o, w, len = 0;
 
@@ -21,13 +22,13 @@ int create_file(const char *filename, char *text_content);
 			len++;
 	}
 
-	o = open(filename, O_CREAT | O_RDWR | 0_TRUNC, 0600);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
-		return (1);
+		return (-1);
 
-	close(0);
+	close(o);
 
 	return (1);
 }
